00ac837588ce05a8c6842414dc783b0468fa7fbd
Dustyn <djtubbs1@svsu.edu>
2016-03-24 09:35:34 -0400

    Added functionality to Tile

--- src/com/dustyn/thunderdome/Tile.java
+++ src/com/dustyn/thunderdome/Tile.java
@@ -2,6 +2,7 @@ package com.dustyn.thunderdome;
 
 import com.dustyn.thunderdome.graphics.Renderable;
 import java.awt.Point;
+import java.lang.Math;
 
 /**
  *
@@ -9,15 +10,24 @@ import java.awt.Point;
  */
 public class Tile implements Renderable{
 
+    static double getDistance(Tile c, Tile tile) {
+           return Math.sqrt(Math.pow((c.getCenter().x + tile.getCenter().x),2) + Math.pow((c.getCenter().y + tile.getCenter().y),2));
+    }
+
     private int[][] tileSprite;
-    Point origin, bounds;
+    Point origin, bounds, center;
     private int color;
+    public double G;
+    public double H;
+    public double F;
+    public Tile parent;
     
     Tile(Point origin, Point bounds) {
         this();
         this.origin = origin;
         this.bounds = bounds;
-        this.color = color;
+        this.center = new Point((origin.x + bounds.x)/2,(origin.y + bounds.y)/2);
+        this.G = this.H = 5000;
     }
 
     public void setColor(int color) {
@@ -34,7 +44,7 @@ public class Tile implements Renderable{
         this.tileSprite = new int[Map.tileWidth][Map.tileWidth];
     }
     
-
+    
 
     @Override
     public int[][] getSprite() {
@@ -50,4 +60,20 @@ public class Tile implements Renderable{
     public Point getBounds() {
         return bounds;
     }
+    
+    public Point getCenter() {
+        return center;
+    }
+    
+    public boolean contains(Point p) {
+        return (origin.x <= p.x && p.x <= bounds.x) &&
+               (origin.y <= p.y && p.y <= bounds.y);
+    }
+    
+    public boolean contains(int x, int y) {
+        return (origin.x <= x && x <= bounds.x) &&
+               (origin.y <= y && y <= bounds.y);
+    }
+
+    
 }
